d5940d33f37bd43f25d2bcaccb3d756df7256588
aleksanderstartsev1984 <strami@yandex.ru>
2024-03-20 17:45:38 +0700

    ...

--- packgagesforpsycoprg2.sh
+++ packgagesforpsycoprg2.sh
@@ -1,4 +1,2 @@
 #!bin/bash
-sudo apt-get install gcc
-sudo apt-get install python-dateutil python-docutils python-feedparser python-gdata python-jinja2 python-ldap python-libxslt1 python-lxml python-mako python-mock python-openid python-psycopg2 python-psutil python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-unittest2 python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi
-sudo apt-get install libxml2-dev libxslt1-dev
+sudo apt-get install gcc python-dateutil python-docutils python-feedparser python-gdata python-jinja2 python-ldap python-libxslt1 python-lxml python-mako python-mock python-openid python-psycopg2 python-psutil python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-unittest2 python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi libxml2-dev libxslt1-dev
