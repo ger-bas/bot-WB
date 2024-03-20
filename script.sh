bf9799333ab802c8852b3aab77b67a0300d6ffbb
aleksanderstartsev1984 <strami@yandex.ru>
2024-03-20 16:05:34 +0700

    ...

--- script.sh
+++ script.sh
@@ -1,2 +1,4 @@
 #!bin/bash
-sudo apt-get install libpq-dev python-dev libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev libffi-dev libjpeg-dev zlib1g-dev
+sudo apt-get install gcc
+sudo apt-get install python-dateutil python-docutils python-feedparser python-gdata python-jinja2 python-ldap python-libxslt1 python-lxml python-mako python-mock python-openid python-psycopg2 python-psutil python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-unittest2 python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi
+sudo apt-get install libxml2-dev libxslt1-dev
