2731d7feea390b8ecce5407535462f0d97315840
aleksanderstartsev1984 <strami@yandex.ru>
2024-03-20 16:00:28 +0700

    ...

--- script.sh
+++ script.sh
@@ -1,4 +1,2 @@
 #!bin/bash
-sudo apt install build-essential autoconf libtool pkg-config python-opengl python-pil python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev libssl-dev
-# sudo easy_install greenlet
-# sudo easy_install gevent
+sudo apt-get install libpq-dev python-dev libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev libffi-dev libjpeg-dev zlib1g-dev
